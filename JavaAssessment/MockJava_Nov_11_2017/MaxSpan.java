a449f426bd0fa674917bca341450ec6867537b24
sairam <sairam1431995@gmail.com>
2017-11-12 15:45:53 +0530

    Update MaxSpan.java
    One Test Case have Failed
    TestCase: intArrayTest, Grade: Failed , Score: 0 out of 2 , Message: Runtime Exception:null

--- JavaAssessment/MockJava_Nov_11_2017/MaxSpan.java
+++ JavaAssessment/MockJava_Nov_11_2017/MaxSpan.java
@@ -1,33 +1,53 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.lang.*;
+
 
 public class MaxSpan {
-
-	public static void main(String[] args) {
-		int[] array={1,22,22,21,8,22,0};
-		System.out.println(findMaxFrequency(array));
-	}
-	public static int findMaxFrequency(int[] arr)
-	{
+	public static int findMaxFrequency(int[] arr) {
+		if("[I"!=arr.getClass().getName())
+		{
+			return 999;
+		}
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i]<=0)
+			{
+				return -1;
+			}
+		}
 		int mainCount=0;
 		int tempCount=0;
 		int currentValue=0;
 		int finalValue=-1;
+		int notValue=0;
 		for(int i=0;i<arr.length-1;i++)
 		{
 			for(int j=i+1;j<arr.length;j++)
 			{
+				if(arr[i]!=notValue)
 				if(arr[i]==arr[j])
 				{
 					currentValue=arr[i];
 					tempCount++;
 				}
 			}
+			notValue=currentValue;
 			if(tempCount>mainCount)
 			{
 				finalValue=currentValue;
 				mainCount=tempCount;
 			}
+			currentValue=0;
+			tempCount=0;
 		}
 		return finalValue;
 	}
-
+	
+	
+	
+	
+	public static void main(String[] args) {
+		int[] arr = {11,23,66,66,2,22,22};
+		System.out.println(findMaxFrequency(arr));
+	}
 }
