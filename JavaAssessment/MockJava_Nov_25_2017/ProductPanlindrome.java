0cdc0be9978cac2ca9dc2e8ae60d403457eb4b92
sairam <sairam1431995@gmail.com>
2017-11-26 17:09:45 +0530

    Update ProductPanlindrome.java

--- JavaAssessment/MockJava_Nov_25_2017/ProductPanlindrome.java
+++ JavaAssessment/MockJava_Nov_25_2017/ProductPanlindrome.java
@@ -5,25 +5,41 @@ public class ProductPanlindrome {
 		final int END = 100;
 
 		System.out.println(ProductPanlindrome.getCount(START, END));
-
 	}
 
 	public static int getCount(int start, int end) {
 		int panlindromeCount=0;
 		for(int i=start;i<end;i++)
 		{
-			for(int j=start+1;j<end;j++)
+			for(int j=start;j<end;j++)
 			{
-				if(isPanlindrome(start,end))
+				if(isPanlindrome(i,j))
 				{
-					
+					panlindromeCount++;
 				}
 			}
 		}
+		return panlindromeCount;
 
 	}
 	public static boolean isPanlindrome(int x,int y)
 	{
-		
+		int multiply=x*y;
+		int original=multiply;
+		int reverse=0;
+		while(multiply!=0)
+		{
+			int digit=multiply%10;
+			reverse=reverse*10+digit;
+			multiply=multiply/10;
+		}
+		if(original==reverse)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
